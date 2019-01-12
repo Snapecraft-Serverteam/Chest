99a01e8f3e7412e3a94d744f5c6c55a7e9380d12
MayusYT <mariusschuh@outlook.de>
2019-01-12 11:23:33 +0100

    Fixed CSS and added much xd

--- player.php
+++ player.php
@@ -18,7 +18,7 @@ if(isset($_GET['p'])) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/player.css">
         <link rel="stylesheet" href="css/searchbar.css">
         <link rel="stylesheet" href="css/alert.css">
         <title>Chest</title>
@@ -28,20 +28,32 @@ if(isset($_GET['p'])) {
     <body>
         <aside>
             <figure>
-                <div id="avatar"></div>
-                <figcaption><img src="https://crafatar.com/renders/body/<?php echo $uuid;?>?scale=6&overlay"><?php echo $name;?></figcaption>
-                </figure>
-                <img id="menuburger" src="images/menu.svg">
-                <nav>
-                    <ul>
-                        <li><a href="index.html" class="current-link">Profil</a></li>
-                        <li><a href="client.html">Minigames</a></li>
-                        <li><a href="#">Modpacks</a></li>
-                        <li><a href="#">Archievements</a></li>
-                    </ul>
-                    
-        
-                </nav>
+                <figcaption>
+                    <div class="uname">
+                        <?php echo $name;?>
+                    </div>
+                     <div class="tag">
+                         <p>Premium</p>
+                     </div>
+                 </figcaption>
+
+                 <div class="avatar">
+                   <img src="https://crafatar.com/renders/body/<?php echo $uuid;?>?scale=6&overlay">
+                 </div>
+                
+            </figure>
+            
+            <img id="menuburger" src="images/menu.svg">
+            <nav>
+                <ul>
+                    <li><a href="index.html" class="current-link">Profil</a></li>
+                    <li><a href="client.html">Minigames</a></li>
+                    <li><a href="#">Modpacks</a></li>
+                    <li><a href="#">Achievements</a></li>
+                </ul>
+                
+    
+            </nav>
                
         </aside>
         <main>
@@ -57,23 +69,7 @@ if(isset($_GET['p'])) {
             
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            <p class="bottom"><?php echo $uuid;?>Dieses Tool wurde von Snapecraft entwickelt und kann auf <a href="https://github.com/MayusYT/Chest">GitHub</a> heruntergeladen werden</p>
+            <p class="bottom">Dieses Tool wurde von Snapecraft entwickelt und kann auf <a href="https://github.com/MayusYT/Chest">GitHub</a> heruntergeladen werden</p>
         
         <script>
         
