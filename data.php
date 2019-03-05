3d7981b51a76aacfe0f88d85038edc60506c8a83
Tallerik <erik.klasse@gmail.com>
2019-03-05 09:28:04 +0100

    Big Update + Need little fixes

--- data.php
+++ data.php
@@ -6,17 +6,28 @@
  */
 
 
+/**
+ * Returns all data
+ * @param $uuid
+ * @return array
+ */
 function getData($uuid) {
     //include 'uuid.php';
     $out = array();
     $out["bw"] = getBWStats(format_uuid($uuid));
     $out["ban"] = getBans(format_uuid($uuid));
-    $out["execBans"] = getExecBans(format_uuid($uuid));
+    //$out["execBans"] = getExecBans(format_uuid($uuid));
+    $out["pt"] = getPlaytime(format_uuid($uuid));
+    $out["online"] = getOnline(uuid_to_username($uuid));
     //print_r($out);
     return $out;
 }
 
 
+/**
+ * @param $uuid
+ * @return mixed
+ */
 function getBWStats($uuid) {
     $host = "localhost";
     $user = "root";
@@ -29,7 +40,7 @@ function getBWStats($uuid) {
     if ($mysqli->connect_errno) {
         die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
     }
-    $id = 100;
+
     $statement = $mysqli->prepare($sql);
     //$statement->bind_param('i', $id);
     $statement->execute();
@@ -37,6 +48,10 @@ function getBWStats($uuid) {
     return $statement->get_result()->fetch_array(MYSQLI_BOTH);
 }
 
+/**
+ * @param $uuid
+ * @return mixed
+ */
 function getBans($uuid) {
     $host = "localhost";
     $user = "root";
@@ -49,7 +64,7 @@ function getBans($uuid) {
     if ($mysqli->connect_errno) {
         die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
     }
-    $id = 100;
+
     $statement = $mysqli->prepare($sql);
     //$statement->bind_param('i', $id);
     $statement->execute();
@@ -58,6 +73,10 @@ function getBans($uuid) {
 
 }
 
+/**
+ * @param $uuid
+ * @return mixed
+ */
 function getExecBans($uuid) {
     $host = "localhost";
     $user = "root";
@@ -70,11 +89,59 @@ function getExecBans($uuid) {
     if ($mysqli->connect_errno) {
         die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
     }
-    $id = 100;
     $statement = $mysqli->prepare($sql);
     //$statement->bind_param('i', $id);
     $statement->execute();
 
     return $statement->get_result()->fetch_array(MYSQLI_BOTH);
 
+}
+
+/**
+ * @param $uuid
+ * @return mixed
+ */
+function getPlaytime($uuid) {
+    $host = "localhost";
+    $user = "root";
+    $pw = "";
+    $db = "playtimetracker";
+
+
+    $servers = array();
+    $out = array();
+
+    $pdo = new PDO('mysql:host=localhost;dbname=playtimetracker', 'root', '');
+    $sql = "SHOW TABLES";
+    $statement = $pdo->prepare($sql);
+    $statement->execute();
+    $tables = $statement->fetchAll(PDO::FETCH_NUM);
+    foreach($tables as $table){
+        //Print the table name out onto the page.
+        array_push($servers, $table[0]);
+    }
+
+
+    foreach ($servers as $server) {
+        $s = "'";
+        $sql = "SELECT * FROM ". $server ." WHERE player=".$s.$uuid.$s.";";
+        $mysqli = new mysqli($host, $user, $pw, $db);
+        if ($mysqli->connect_errno) {
+            die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
+        }
+        $statement = $mysqli->prepare($sql);
+        //$statement->bind_param('i', $id);
+        $statement->execute();
+        $out[$server] = $statement->get_result()->fetch_array(MYSQLI_BOTH);
+        $out[$server]["name"] = $server;
+    }
+    return $out;
+
+
+}
+
+function getOnline($name) {
+    $req = file_get_contents("http://localhost:8000/isplayeronline?player=$name");
+    $data = json_decode($req, true);
+    return $data;
 }
--- player.php
+++ player.php
@@ -8,6 +8,23 @@ if(isset($_GET['p'])) {
         $name = $_GET['p'];
         $uuid = username_to_uuid($name);
         $data = getData($uuid);
+        $tag = array();
+        $tag["color"] = "";
+        $tag["content"] = "";
+        if($data['ban']['uuid'] != "") { // Banned
+            $tag["color"] = "red";
+            $tag["content"] = "Gebannt";
+        } else {
+             if($data["online"]["online"] == "true") {
+                 $tag["color"] = "green";
+                 $server = $data["online"]["server"];
+                 $tag["content"] = "Spielt auf ".$server;
+             } else {
+                 $tag["color"] = "gray";
+                 $tag["content"] = "Offline";
+             }
+        }
+
 }
 
 
@@ -22,8 +39,6 @@ if(isset($_GET['p'])) {
         <link rel="stylesheet" href="css/searchbar.css">
         <link rel="stylesheet" href="css/alert.css">
         <title>Chest</title>
-        
-        
     </head>
     <body>
         <aside>
@@ -32,17 +47,14 @@ if(isset($_GET['p'])) {
                     <div class="uname">
                         <?php echo $name;?>
                     </div>
-                     <div class="tag">
-                         <p>Premium</p>
+                     <div class="tag" style="background-color: <?php echo $tag["color"];?>;">
+                         <p><?php echo $tag["content"];?></p>
                      </div>
                  </figcaption>
-
                  <div class="avatar">
                    <img src="https://crafatar.com/renders/body/<?php echo $uuid;?>?scale=6&overlay">
                  </div>
-                
             </figure>
-            
             <img id="menuburger" src="images/menu.svg">
             <nav>
                 <ul>
@@ -51,8 +63,6 @@ if(isset($_GET['p'])) {
                     <li><a href="#">Modpacks</a></li>
                     <li><a href="#">Achievements</a></li>
                 </ul>
-                
-    
             </nav>
                
         </aside>
@@ -69,11 +79,11 @@ if(isset($_GET['p'])) {
 
 
             <header>
-
+<!--                background-color: #D4913A;-->
                 <div class="titleName">
                     <?php echo $name;?>
-                    <div class="titleTag">
-                        <p>Premium</p>
+                    <div class="titleTag" style="background-color: <?php echo $tag["color"];?>;">
+                        <p><?php echo $tag["content"];?></p>
                     </div>
                 </div>
                 <div class="status">
@@ -85,21 +95,46 @@ if(isset($_GET['p'])) {
 
 
 
-
-
-
-
-
-            <div class="world-box" style="-webkit-font-smoothing: subpixel-antialiased">
+            <!--Bedwars-->
+            <?php
+                $bw = $data["bw"];
+            ?>
+            <div class="world-box" id="bw" style="-webkit-font-smoothing: subpixel-antialiased">
                 <div class="world-box-titlebar">
                     <p class="world-box-titlebar-text"><b>Bedwars</b></p>
                 </div>
                 <div class="world-box-body">
-                    <p><b>Kills:</b> <?php echo $row[''] ?></p>
-                    <p>Mitspieler: LordBuschos, Tallerik</p><br>
-                    <p>Spielstunden: 71</p>
+                    <p><b>Wins:</b> <?php echo $bw['wins'] ?></p>
+                    <p><b>Betten zerstört:</b> <?php echo $bw['destroyedBeds'] ?></p>
+                    <p><b>Loses:</b> <?php echo $bw['loses'] ?></p>
+                    <p><b>Kills:</b> <?php echo $bw['kills'] ?></p>
+                    <p><b>Deaths:</b> <?php echo $bw['deaths'] ?></p>
+                    <p><b>Score:</b> <?php echo $bw['score'] ?></p>
                 </div>
             </div>
+
+            <!--Playtime-->
+            <?php
+            $playtime = $data["pt"];
+            if(sizeof($playtime) != 0) {
+            ?>
+                <div class="world-box" id="bw" style="-webkit-font-smoothing: subpixel-antialiased">
+                    <div class="world-box-titlebar">
+                        <p class="world-box-titlebar-text"><b>Spielzeit</b></p>
+                    </div>
+                    <div class="world-box-body">
+                        <?php
+                        foreach($playtime as $server) {
+
+                         echo "<p><b>".$server["name"].":</b> ".date("m", $server["playtime"] / 1000)." Minuten</p>";
+
+                        }
+                        ?>
+                    </div>
+                </div>
+            <?php } ?>
+
+
             <p class="bottom">Dieses Tool wurde von Snapecraft entwickelt und kann auf <a href="https://github.com/MayusYT/Chest">GitHub</a> heruntergeladen werden</p>
         </main>
 
@@ -107,11 +142,10 @@ if(isset($_GET['p'])) {
 
         <script type="text/javascript" src="vanilla-tilt.js"></script>
         <script>
-            VanillaTilt.init(document.querySelector(".world-box"), {
+            VanillaTilt.init(document.querySelector("#bw"), {
                 max: 8,
                 speed: 50
             });
-            
         </script>
         
     </body>
